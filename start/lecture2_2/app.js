4ae282a5f694e6d709383a349c4ca7ed2ab01917
Walter Clayton <walter.h.clayton@gmail.com>
2022-05-24 05:51:52 +0200

    corrected camera perspective

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -7,6 +7,7 @@ class App{
 		document.body.appendChild( container );
     
         this.camera = new THREE.PerspectiveCamera (60, window.innerWidth, window.innerHeight, 0.1, 100);
+		
         this.camera.position.set(0, 0, 4);
 
 		this.scene = new THREE.Scene();
